b7e9a69b842a7f4036e474e9ddf075d3c8258a99
joeytall <joeytall.liu@gmail.com>
2014-03-28 00:23:43 -0700

    add zprezto as submodule

---
+++ home/.zprezto/modules/tmux/init.zsh
@@ -0,0 +1,43 @@
+#
+# Defines tmux aliases and provides for auto launching it at start-up.
+#
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#   Colin Hebert <hebert.colin@gmail.com>
+#   Georges Discry <georges@discry.be>
+#   Xavier Cambar <xcambar@gmail.com>
+#
+
+# Return if requirements are not found.
+if (( ! $+commands[tmux] )); then
+  return 1
+fi
+
+#
+# Auto Start
+#
+
+if [[ -z "$TMUX" && -z "$EMACS" && -z "$VIM" ]] && ( \
+  ( [[ -n "$SSH_TTY" ]] && zstyle -t ':prezto:module:tmux:auto-start' remote ) ||
+  ( [[ -z "$SSH_TTY" ]] && zstyle -t ':prezto:module:tmux:auto-start' local ) \
+); then
+  tmux_session='prezto'
+
+  # Create a first 'prezto' session if tmux is starting.
+  if ! tmux has-session 2> /dev/null; then
+    tmux \
+      start-server \; \
+      new-session -d -s "$tmux_session" \; \
+      set-option -t "$tmux_session" destroy-unattached off &> /dev/null
+  fi
+
+  # Attach to the 'prezto' session or to the last session used.
+  exec tmux attach-session
+fi
+
+#
+# Aliases
+#
+
+alias tmuxa='tmux attach-session'
+alias tmuxl='tmux list-sessions'
